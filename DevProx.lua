5495303c7c1baf6dd7237ad9643fc1f1d7a8a3f7
iq0abs <62826664+iq0abs@users.noreply.github.com>
2020-04-12 12:17:51 +0300

    Update DevProx.lua

--- DevProx.lua
+++ DevProx.lua
@@ -11292,7 +11292,6 @@ getMessage(msg.chat_id_, msg.message_id_,get_msg_contact)
 if (data.ID == "UpdateOption" and data.name_ == "my_id") then
 tdcli_function ({ID="GetChats", offset_order_="9223372036854775807", offset_chat_id_=0, limit_=20}, dl_cb, nil)
 end
-end 
  ----------------------------------------------
  -- This Source Was Developed By (ABS) @IQ_ABS.
  -- This Is The Source Channel @Dev_Prox .
