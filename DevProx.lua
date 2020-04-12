49d8af4f1071a17edf68f36fac4cc466f2b4f2cd
iq0abs <62826664+iq0abs@users.noreply.github.com>
2020-04-12 12:07:27 +0300

    Update DevProx.lua

--- DevProx.lua
+++ DevProx.lua
@@ -11289,7 +11289,7 @@ delete_msg(msg.chat_id_,msgs)
 end end end end
 
 getMessage(msg.chat_id_, msg.message_id_,get_msg_contact)
-elseif (data.ID == "UpdateOption" and data.name_ == "my_id") then
+if (data.ID == "UpdateOption" and data.name_ == "my_id") then
 tdcli_function ({ID="GetChats", offset_order_="9223372036854775807", offset_chat_id_=0, limit_=20}, dl_cb, nil)
 end end
  ----------------------------------------------
