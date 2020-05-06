f6334ebd1c689e33bab1b6688c48a009695e9246
iq0abs <62826664+iq0abs@users.noreply.github.com>
2020-05-06 04:02:54 +0300

    Update DevProx.lua

--- DevProx.lua
+++ DevProx.lua
@@ -12245,7 +12245,7 @@ local chtext = DevAbs:get(DevProx.."bot:textch:user")
 if chtext then
 Dev_Abs(msg.chat_id_, msg.id_, 1, '❗️🚸 ⌯ كڵيشة ٱلٱشترٱك ٱلٱجبٱري : \n〰️➖〰️➖〰️➖〰️➖〰️\n['..chtext..']', 1, 'md')
 else
-Dev_Abs(msg.chat_id_, msg.id_, 1, '\n❗️☻ عُـذراً لايُمكِنك ٱستخدام ٱڵـبوت \nلانك لم تشترك فيۧ قناة ٱڵـبوت \n❗️🚸 رجائاً ٱشترك  بٱڵـقناة ڵـتتمكن من ٱستخدٱمة. \n❗️📍 قناة ٱڵـبوت : { ['..channel..'] }', 1, 'md')
+Dev_Abs(msg.chat_id_, msg.id_, 1, "\n❗️☻ عُـذراً لايُمكِنك ٱستخدام ٱڵـبوت \nلانك لم تشترك فيۧ قناة ٱڵـبوت \n❗️🚸 رجائاً ٱشترك  بٱڵـقناة ڵـتتمكن من ٱستخدٱمة. \n❗️📍 قناة ٱڵـبوت : { ["..channel.."] }", 1, "md")
 end end end
 --     Source DevProx     --
 if text:match("^القناة$") or text:match("^قناة السورس$") or text:match("^قنات السورس$") then
