93c1e672d445ee076ea1f3ca1a2bf20f9a428fcb
iq0abs <62826664+iq0abs@users.noreply.github.com>
2020-05-10 06:05:48 +0300

    Update DevProx.lua

--- DevProx.lua
+++ DevProx.lua
@@ -4839,7 +4839,7 @@ end
 --     Source DevProx     --
 if text:match("(.*)(ضافني)(.*)") then
 if not DevAbs:get(DevProx..'lock:Added:Me'..msg.chat_id_) then
-tdcli_function ({ID = "GetChatMember",chat_id_ = msg.chat_id_,user_id_ = msg.sender_user_id_},function(arg,da) 
+tdcli_function ({ID = "GetChatMember",chat_id_ = msg.chat_id_,user_id_ = msg.sender_user_id_},function(arg,da),nil)
 if da and da.status_.ID == "ChatMemberStatusCreator" then
 Dev_Abs(msg.chat_id_, msg.id_, 1, 'انت منشئ المجموعه ', 1, 'md')
 else
@@ -4848,14 +4848,14 @@ end
 end
 local Added_Me = DevAbs:get(DevProx.."bot:Who:Added:Me"..msg.chat_id_..':'..msg.sender_user_id_)
 if Added_Me then 
-tdcli_function ({ID = "GetUser",user_id_ = Added_Me},function(extra,result,success)
+tdcli_function ({ID = "GetUser",user_id_ = Added_Me},function(extra,result,success),nil)
 local Name = '['..result.first_name_..'](tg://user?id='..result.id_..')'
 Text = 'الشخص الذي قام باضافتك هو » '..Name
 Dev_Abs(msg.chat_id_,Text,msg.id_/2097152/0.5,'md')
-end,nil)
+end
 else
 Dev_Abs(msg.chat_id_, msg.id_, 1, 'انت دخلت عبر الرابط', 1, 'md')
-end,nil)
+end
 else
 Dev_Abs(msg.chat_id_, msg.id_, 1, 'امر منو ضافني تم تعطيله من قبل المدراء ', 1, 'md')
 end
