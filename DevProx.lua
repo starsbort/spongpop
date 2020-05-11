8ba0de804ff3e597115df54bc216a624469f37ff
iq0abs <62826664+iq0abs@users.noreply.github.com>
2020-05-12 02:34:26 +0300

    Update DevProx.lua

--- DevProx.lua
+++ DevProx.lua
@@ -1732,6 +1732,8 @@ function adding(extra,result,success)
 if not DevAbs:get(DevProx.."bot:leader:pv"..msg.chat_id_) then
 local users = DevAbs:scard(DevProx.."bot:userss")
 Dev_Abs(tostring((DevAbs:get(DevProx.."bot:leader:gr") or bot_owner)), 0, 1, "❗️🎒 ⌯ هنٱك مشترك جديد في ٱڵبوت \n〰️➖〰️➖〰️➖〰️➖〰️\n❗️🚸 ⌯ معرفه ⌯» ◝ @"..(result.username_ or "لا يوجد").." ◟\n❗️🔑 ⌯ ٱيديه ⌯» ◝ "..msg.sender_user_id_.." ◟\n❗️🔅 ⌯ عدد مشتركين ٱڵبوت ⌯» ◝ "..users.." ◟" , 1, 'html') 
+else
+Dev_Abs(tostring((DevAbs:get(DevProx.."bot:leader:gr") or bot_owner)), 0, 1, "" , 1, 'html') 
 end 
 getUser(msg.sender_user_id_,adding) 
 end
@@ -1742,6 +1744,8 @@ if text and text:match("/start hms(.*)_(%d+)") then
 function adding(extra,result,success)
 if not DevAbs:get(DevProx.."bot:leader:pv"..msg.chat_id_) then
 Dev_Abs(tostring((DevAbs:get(DevProx.."bot:leader:gr") or bot_owner)), 0, 1, "❗️🎒 ⌯ هذٱ دزوله همسه وديشوفهٱ \n〰️➖〰️➖〰️➖〰️➖〰️\n❗️🚸 ⌯ معرفه ⌯» ◝ @"..(result.username_ or "لا يوجد").." ◟\n❗️🔑 ⌯ ٱيديه ⌯» ◝ "..msg.sender_user_id_.." ◟" , 1, 'html') 
+else 
+Dev_Abs(tostring((DevAbs:get(DevProx.."bot:leader:gr") or bot_owner)), 0, 1, "" , 1, 'html') 
 end 
 getUser(msg.sender_user_id_,adding) 
 end 
